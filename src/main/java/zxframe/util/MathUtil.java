1631321d493b4df00c80de6cedb80602ea24c811
zhouxuanGithub <1164429680@qq.com>
2018-07-29 19:10:34 +0800

    工具类提交

---
+++ src/main/java/zxframe/util/MathUtil.java
@@ -0,0 +1,33 @@
+package zxframe.util;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Random;
+
+/**
+ * 计算util
+ * @author 周璇
+ *
+ */
+public class MathUtil {
+	/**
+	 * 用于计算成功几率
+	 * @param v double0-1
+	 * @return
+	 * @throws ParseException
+	 */
+	public static boolean isSuccess(String v) throws ParseException {
+		return isSuccess((double)NumberFormat.getPercentInstance().parse(v));
+	}
+	/**
+	 * 用于计算成功几率
+	 * @param v double0-1
+	 * @return
+	 */
+	public static boolean isSuccess(double v) {
+		if(new Random().nextInt(100)+1<=v*100) {
+			return true;
+		}
+		return false;
+	}
+}
