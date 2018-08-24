d6b22ea76b1f2b05668f9e49bca50034d405865e
zhouxuanGithub <1164429680@qq.com>
2018-08-24 14:37:15 +0800

    方法级缓存备份

---
+++ src/main/java/zxframe/cache/annotation/ServiceFnCache.java
@@ -0,0 +1,13 @@
+package zxframe.cache.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//方法级数据缓存
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ServiceFnCache {
+	String group() default "zxframe-fdcache-default";
+}
