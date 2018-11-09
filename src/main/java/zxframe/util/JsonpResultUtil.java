43dd08c80105209e0497e936f5443674552551d4
zhouxuanGithub <1164429680@qq.com>
2018-11-09 11:44:24 +0800

    提交工具类

---
+++ src/main/java/zxframe/util/JsonpResultUtil.java
@@ -0,0 +1,23 @@
+package zxframe.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import zxframe.util.JsonUtil;
+
+public class JsonpResultUtil {
+	public static void print(HttpServletRequest request,HttpServletResponse response, Object obj) throws IOException {
+		 PrintWriter writer = response.getWriter();
+		 if(request.getParameter("callback")!=null){
+			 writer.print(request.getParameter("callback")+"("+JsonUtil.obj2Json(obj)+");");
+		 }else {
+			 writer.print(JsonUtil.obj2Json(obj));
+		 }
+		 writer.flush();
+		 writer.close();
+	}
+}
+
