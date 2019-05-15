05c5412988f69b376bdcc3bfc7133a0f65b3b4e2
zhouxuanGithub <1164429680@qq.com>
2019-05-15 18:32:28 +0800

    调整可用和不可用文件

---
+++ demo/其他可用类参考/RestTemplateConfig.java
@@ -0,0 +1,27 @@
+package com.voole.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+ 
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        return factory;
+    }
+}
+//使用
+//@Autowired
+//private RestTemplate restTemplate;
+//restTemplate.getForObject("url", String.class);
