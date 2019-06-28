ae0d8e56200926f159a4637ec599ad3bfc6ea585
Administrator <Administrator@Lenovo-PCyp>
2019-06-28 18:16:22 +0800

    demo编写

---
+++ demo/webproject/src/main/java/zxframe/demo/Application.java
@@ -0,0 +1,18 @@
+package zxframe.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import zxframe.ZxFrameComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+@ZxFrameComponentScan
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
