a5108624613d71a89707b4519b46a06a65662cc2
Administrator <Administrator@Lenovo-PCyp>
2019-07-02 17:45:49 +0800

    删除无用代码

--- src/main/java/zxframe/config/ZxFrameConfig.java
+++ src/main/java/zxframe/config/ZxFrameConfig.java
@@ -2,7 +2,6 @@ package zxframe.config;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
