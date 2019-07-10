1eb61f73783bee1fc6c22a332d5455bc5f32d080
Administrator <Administrator@Lenovo-PCyp>
2019-07-10 14:09:00 +0800

    调整课程11

--- demo/webproject/src/main/java/zxframe/demo/lesson11/service/Lesson11Service.java
+++ demo/webproject/src/main/java/zxframe/demo/lesson11/service/Lesson11Service.java
@@ -16,7 +16,8 @@ public class Lesson11Service {
 	private CacheManager cacheManager;
 	public Object doTest() {
 		String id="79cbc702-9092-4042-b790-5e9523661b8c";
-		User11 user11 = mysqlTemplate.get(User11.class, "79cbc702-9092-4042-b790-5e9523661b8c");//会添加or更新库数据和对应缓存
+		User11 user11 =null;
+		user11 = mysqlTemplate.get(User11.class, id);//会添加or更新库数据和对应缓存
 		//mysqlTemplate.delete(clas, id);//会删除库数据和对应缓存
 		//mysqlTemplate.update(obj);//会添加or更新库数据和对应缓存
 		//mysqlTemplate.save(obj)//会添加or更新库数据和对应缓存
@@ -25,6 +26,6 @@ public class Lesson11Service {
 	}
 	public Object doTest2() {
 		//cacheManager.removeQueryCache("lesson11MapperSelect");//清理查询缓存
-		return mysqlTemplate.get("lesson11MapperSelect");//结果会加入缓存
+		return mysqlTemplate.getList("lesson11MapperSelect");//结果会加入缓存
 	}
 }
