f45f671a106722973ce74d22784a2470e347d87b
zhouxuanGithub <1164429680@qq.com>
2019-07-31 18:39:08 +0800

    错误日志展示优化

--- src/main/java/zxframe/controller/ZxframeController.java
+++ src/main/java/zxframe/controller/ZxframeController.java
@@ -15,41 +15,40 @@ public class ZxframeController {
 	@Value("${server.tomcat.basedir}")
 	private String basedir;
 	private static long ctime=0;
-	//查看错误日志
+	//查看运行状态
 	@RequestMapping("error")
 	public synchronized String error() {
 		if(!checkRunTime()) {
 			return "访问频率太快，请稍等一下！";
 		}
-		return "<pre>"+SystemUtil.exec("tail -n 500 "+basedir+"log/error."+new DateUtil("yyyy-MM-dd").getDate()+".log")+"</pre>";
+		String top="<meta charset=\"utf-8\"/><body onLoad='window.document.body.scrollTop = document.body.scrollHeight;'><pre><xmp>";
+		String content=SystemUtil.exec("tail -n 500 "+basedir+"log/error."+new DateUtil("yyyy-MM-dd").getDate()+".log");
+		String end="</xmp></pre><hr/>"+status()+"</body>";
+		return top+content+end;
 	}
 	//查看运行状态
 	@RequestMapping("status")
-	public synchronized String status() {
-		if(!checkRunTime()) {
-			return "访问频率太快，请稍等一下！";
-		}
+	private synchronized String status() {
 		StringBuffer sb=new StringBuffer();
-	    sb.append("<br><br>");  
+		sb.append("<meta charset=\"utf-8\"/>");
 	    long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024; //java虚拟机能取得的最大内存  
 	    long totalMemory = Runtime.getRuntime().totalMemory() / 1024 / 1024; //java虚拟机当前取得的内存大小  
 	    long freeMemory = Runtime.getRuntime().freeMemory() / 1024 / 1024; //java虚拟机所占用的内存中的空闲部分  
 	    long usedMemory = totalMemory - freeMemory; //java虚拟机当前实际使用的内存大小  
+	    if (usedMemory < maxMemory) {  
+	        sb.append("内存使用状况：正常"); //标记字符!  
+	    } else {  
+	        sb.append("内存使用状况：溢出"); //标记字符!  
+	    }  
 	    sb.append("<br>虚拟机能取得的最大内存 : " + maxMemory + "M");  
 	    sb.append("<br>虚拟机当前取得的内存大小: " + totalMemory + "M");  
 	    sb.append("<br>虚拟机当前实际使用的内存大小  : " + usedMemory + "M");  
 	    sb.append("<br>虚拟机所占用的内存中的空闲部分 : " + freeMemory + "M");  
-	    sb.append("<br><br>");  
-	  
-	    if (usedMemory < maxMemory) {  
-	        sb.append("内存使用状况正常"); //标记字符!  
-	    } else {  
-	        sb.append("内存溢出"); //标记字符!  
-	    }  
-	    sb.append("<br><br>");  
-	    sb.append(new java.util.Date());  
 	    sb.append("<br>");  
-	    sb.append(TimeZone.getDefault().getDisplayName());  
+	    sb.append(new java.util.Date());  
+	    sb.append("<br>");
+	    sb.append(TimeZone.getDefault().getDisplayName());
+	    sb.append("<br>");
 		return sb.toString();
 	}
 	private boolean checkRunTime() {
