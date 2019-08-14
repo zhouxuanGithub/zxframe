7029c47e0ab04e5b413f6d679b58a1641dde91bc
zhouxuanGithub <1164429680@qq.com>
2019-08-14 16:46:27 +0800

    设置utf-8

--- demo/webproject/src/main/java/zxframe/sys/config/RestTemplateConfig.java
+++ demo/webproject/src/main/java/zxframe/sys/config/RestTemplateConfig.java
@@ -1,9 +1,12 @@
 package zxframe.sys.config;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,13 +14,15 @@ public class RestTemplateConfig {
  
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory){
-        return new RestTemplate(factory);
+    	RestTemplate restTemplate = new RestTemplate(factory);
+    	restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
     }
     @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000);
-        factory.setReadTimeout(5000);
+        factory.setReadTimeout(5000);        
         return factory;
     }
 }
