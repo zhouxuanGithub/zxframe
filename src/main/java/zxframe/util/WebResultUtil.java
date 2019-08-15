c95e7d75c4192f6645a4c64088df9e6a704e4148
zhouxuanGithub <1164429680@qq.com>
2019-08-15 17:20:53 +0800

    http上下文调整

--- src/main/java/zxframe/util/WebResultUtil.java
+++ src/main/java/zxframe/util/WebResultUtil.java
@@ -6,32 +6,21 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import zxframe.config.ZxFrameConfig;
+import zxframe.http.Context;
 
 public class WebResultUtil {
-	private static Logger logger = LoggerFactory.getLogger(WebResultUtil.class);
-	public static ThreadLocal<HttpServletRequest> currentRequest = new ThreadLocal<HttpServletRequest>();
-	public static ThreadLocal<HttpServletResponse> currentResponse = new ThreadLocal<HttpServletResponse>();
 	public static void print(HttpServletRequest request,HttpServletResponse response, Object obj) throws IOException {
 		 if(request==null) {
-		 	request= currentRequest.get();
+		 	request= Context.currentRequest.get();
 		 }
 		 if(response==null) {
-			response=currentResponse.get();
+			response=Context.currentResponse.get();
 		 }
 		 response.setContentType("text/html; charset=utf-8");
 		 PrintWriter writer = response.getWriter();
 		 String r=JsonUtil.obj2Json(obj);
 		 if(request.getParameter("callback")!=null){
 			 r=request.getParameter("callback")+"("+r+");";
-		 }
-		 if(ZxFrameConfig.showlog) {
-			 if(request.getRequestURI().indexOf("/zxframe/")==-1) {
-				 logger.info("web result : "+r);
-			 }
 		 }
 		 writer.print(r);
 		 writer.flush();
