26e730c8fd7533c208677afb58dbae1c71311992
zhouxuanGithub <1164429680@qq.com>
2019-08-19 10:41:54 +0800

    注释调整

--- src/main/java/zxframe/log/annotation/FnLog.java
+++ src/main/java/zxframe/log/annotation/FnLog.java
@@ -1,3 +1,20 @@
+/**
+ * ZxFrame Java Library
+ * https://github.com/zhouxuanGithub/zxframe
+ *
+ * Copyright (c) 2019 zhouxuan
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package zxframe.log.annotation;
 
 import java.lang.annotation.ElementType;
@@ -5,6 +22,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *	方法执行日志记录。
+ *	放在类上可记录此类所有方法的执行日志。
+ *	放在方法上可记录此方法的执行日志。
+ */
 @Target({ElementType.METHOD,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FnLog {
