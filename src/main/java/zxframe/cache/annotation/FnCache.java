25c8e0de9f88fc564be3223d3690dea8d70ccee1
zhouxuanGithub <1164429680@qq.com>
2019-08-19 10:43:30 +0800

    日志调整

--- src/main/java/zxframe/cache/annotation/FnCache.java
+++ src/main/java/zxframe/cache/annotation/FnCache.java
@@ -23,7 +23,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 //方法级数据缓存
-//不在@Service中使用，单独建立cache目录使用
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FnCache {
