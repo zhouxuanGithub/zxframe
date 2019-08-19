7d4db46b20c137e475fce6416205be808550e529
zhouxuanGithub <1164429680@qq.com>
2019-08-19 17:12:33 +0800

    注释调整

--- demo/webproject/src/main/java/zxframe/demo/other/SqlParse.java
+++ demo/webproject/src/main/java/zxframe/demo/other/SqlParse.java
@@ -2,7 +2,7 @@ package zxframe.demo.other;
 
 import zxframe.jpa.inf.ISQLParse;
 
-//QL在执行前，会将执行SQL传给此类，可自行再加工或者分析
+//SQL在执行前，会将执行SQL传给此类，可自行再加工或者分析
 public class SqlParse implements ISQLParse{
 
 	@Override
