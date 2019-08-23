e6ccdffe37624a098c4cf81551d47536b09baf27
zhouxuanGithub <1164429680@qq.com>
2019-08-23 14:52:27 +0800

    调整注释

--- demo/webproject/src/main/java/zxframe/demo/other/SqlParse.java
+++ demo/webproject/src/main/java/zxframe/demo/other/SqlParse.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import zxframe.jpa.inf.ISQLParse;
 
 //SQL在执行前，会将执行SQL传给此类，可自行再加工或者分析
+//在zxframe.xml里配置此类的spring bean id
 @Component
 public class SqlParse implements ISQLParse{
 
