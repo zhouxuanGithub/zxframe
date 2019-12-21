387719cbe16953d895116487223ea4eb444f0797
zhouxuanGithub <1164429680@qq.com>
2019-12-21 13:08:09 +0800

    删除无用代码

--- src/main/java/zxframe/aop/ServiceAspect.java
+++ src/main/java/zxframe/aop/ServiceAspect.java
@@ -25,8 +25,6 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import zxframe.cache.transaction.CacheTransaction;
